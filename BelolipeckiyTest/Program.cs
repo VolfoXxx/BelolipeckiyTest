3fb66aeb8862b99e8c192961a997130c7d515f61
VolfoXxx <118421435+VolfoXxx@users.noreply.github.com>
2022-11-30 14:31:09 +0900

    Add files via upload

--- BelolipeckiyTest/Program.cs
+++ BelolipeckiyTest/Program.cs
@@ -13,12 +13,64 @@ namespace BelolipeckiyTest
         static void Main(string[] args)
         {
             FunctionInsp function = new FunctionInsp();
+
             function.GetInspector();
             function.GetCarInspection();
-            function.SetInspector();
-            function.GenerateNumber();
-            function.GetWorker();
-            /*function.AddWorker();*/
+
+            Begin();
+            
+            
+            Console.ReadKey();
+        }
+
+        static void Begin()
+        {
+            FunctionInsp function = new FunctionInsp();
+
+            Console.WriteLine("Выберите действие:\n1 - Изменить Главного инспектора\n2 - Сгенерировать автомобильный номер\n3 - Вывести список сотрудников\n4 - Добавить сотрудников");
+
+            Middle();
+        }
+
+        static void Middle()
+        {
+
+            FunctionInsp function = new FunctionInsp();
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            if (a == 1)
+            {
+                function.SetInspector();
+                Begin();
+            }
+            else if (a == 2)
+            {
+                function.GenerateNumber();
+                Begin();
+            }
+            else if (a == 3)
+            {
+                function.GetWorker();
+                Begin();
+            }
+            else if (a == 4)
+            {
+                function.AddWorker();
+                Begin();
+            }
+            else
+            {
+                End();
+            }
+        }
+
+        static void End()
+        {
+            Console.Clear();
+            Begin();
         }
     }
 }
